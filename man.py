68302153ddf4ade7cf0817dd32ac90eb3abcb12f
ian otieno <ianotieno23@gmail.com>
2023-09-06 00:40:03 +0300

    update

--- man.py
+++ man.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, CHAR
+from sqlalchemy import create_engine, Column, String, Integer, CHAR,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -24,6 +24,21 @@ class Person(Base):
     def __repr__(self):
         return f"Person({self.ssn}, {self.firstname}, {self.lastname}, {self.gender}, {self.age})"
 
+class Thing(Base):
+    __tablename__ ="things"
+
+    tid=Column("tid",Integer,primary_key=True)
+    description=Column("description",String)
+    owner =Column(Integer,ForeignKey("people.ssn"))
+
+    def __init__(self,tid,description,owner):
+        self.tid=tid
+        self.description=description
+        self.owner=owner
+
+    def __repr__ (self):
+        return f"({self.tid},{self.description},owned by {self.owner})"    
+
 
 engine = create_engine('sqlite:///mydb.db', echo=True)
 Base.metadata.create_all(bind=engine)
@@ -35,16 +50,20 @@ session = Session()
 # session.add(person)
 # session.commit()
 
-# p1 = Person(987, "ian", "smith", "m", 23)
+p1 = Person(912, "oti", "smith", "m", 23)
 # p2 = Person(564, "paul", "smith", "f", 23)
 # p3 = Person(533, "meek", "juger", "m", 23)
-# session.add(p1)
+session.add(p1)
 # session.add(p2)
 # session.add(p3)
-# session.commit()
+session.commit()
 # new_person = Person(767, "John", "Doe", "m", 25)
 # session.add(new_person)
 # session.commit()
+
+t1=Thing(1,"car",p1.ssn)
+session.add(t1)
+session.commit()
 
 results = session.query(Person).all()
 print(results)
